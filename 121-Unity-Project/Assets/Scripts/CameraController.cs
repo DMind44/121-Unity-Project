eee473aa25a0ad469074b2f6db09b8495a8a88e5
how4rd <15080656+how4rd@users.noreply.github.com>
2020-02-18 20:35:04 -0800

    feat(camera): rotate camera to face player direction

--- 121-Unity-Project/Assets/Scripts/CameraController.cs
+++ 121-Unity-Project/Assets/Scripts/CameraController.cs
@@ -9,18 +9,20 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject player;
+    public float cameraRotationSpeed;
+
+    public float heightAbovePlayer;
+    public float distanceBehindPlayer;
 
     private Vector3 playerToCameraOffset;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerToCameraOffset = transform.position - player.transform.position;
-    }
 
     // Update the camera position to track the player
     void LateUpdate()
     {
-        transform.position = player.transform.position + playerToCameraOffset;
+        // place the camera behind and above the player
+        transform.position = player.transform.position - (distanceBehindPlayer * player.transform.forward) + (Vector3.up * heightAbovePlayer);
+
+        // rotate the camera to look the same way the player is
+        transform.LookAt(player.transform.position);
     }
 }
