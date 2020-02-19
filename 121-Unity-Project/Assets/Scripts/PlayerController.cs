64b6e6adc284201b1989afd4b17aa487eab97419
how4rd <15080656+how4rd@users.noreply.github.com>
2020-02-18 19:37:11 -0800

    feat(player movement): translate character based on keyboard input

---
+++ 121-Unity-Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Adapted from the Roll-a-Ball tutorial at
+// https://learn.unity.com/project/roll-a-ball-tutorial
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Perform physics updates at regular time intervals
+    void FixedUpdate()
+    {
+        // Move player in response to input
+        float horizontalMovementInput = Input.GetAxis("Horizontal");
+        float verticalMovementInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalMovementInput, 0, verticalMovementInput);
+        rb.AddRelativeForce(movement * movementSpeed);
+    }
+
+}
