3bcafda9cef74d6a17be689b7c182707a4622280
how4rd <15080656+how4rd@users.noreply.github.com>
2020-02-18 21:09:39 -0800

    feat(pickups): add interactable object that changes color on hover

---
+++ 121-Unity-Project/Assets/Scripts/Interactable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Adapted from Unity docs:
+// https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnMouseOver.html
+
+public class Interactable : MonoBehaviour
+{
+
+    private Color originalColor;
+    private Color hoverColor = Color.green;
+    private MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        originalColor = meshRenderer.material.color;
+    }
+
+    void OnMouseOver()
+    {
+        print("you're hovering over an interactable!");
+        meshRenderer.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        meshRenderer.material.color = originalColor;
+    }
+}
