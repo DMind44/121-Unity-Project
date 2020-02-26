d87e5285603756d28db7583fcbb367904cf2c6de
how4rd <15080656+how4rd@users.noreply.github.com>
2020-02-26 12:02:14 -0800

    feat(menu): add basic main menu to host/join game

---
+++ 121-Unity-Project/Assets/Scripts/MainMenuUIHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class MainMenuUIHandler : MonoBehaviour {
+
+    public NetworkManager manager;  // object that we manipulate to perform networking tasks
+
+    // When the user clicks the host button, create a room
+    public void HostRoom() {
+        manager.StartServer();
+        print("hosting!");
+    }
+
+    // When the user clicks the join button, join an existing room
+    public void JoinRoom() {
+        manager.StartClient();
+        manager.networkAddress = "localhost";
+    }
+}
