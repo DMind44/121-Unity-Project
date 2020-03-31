02f5059c9156ca07c2bf1a341ab3a70a4f955a96
serenity-wade <swade@hmc.edu>
2020-03-31 11:14:00 -0700

    feat(Incomplete): Implement pause menu, needs network compatibility

---
+++ 121-Unity-Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+
+    void Start() {
+        pauseMenuUI.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Debug.Log("Escape pressed!");
+            if (GamePaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() 
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause() 
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void LoadMenu() {
+        Debug.Log("Loading Menu");
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quitting Game");
+    }
+}
