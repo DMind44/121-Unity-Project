15673b81d213082bb1afc841ec554a8bca23a081
aely-aronoff <aely.aro@gmail.com>
2020-03-30 20:00:36 -0700

    feat(Camera): Zoom camera in and out with mouse wheel

--- 121-Unity-Project/Assets/Scripts/CameraController.cs
+++ 121-Unity-Project/Assets/Scripts/CameraController.cs
@@ -6,39 +6,69 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject player = null;
 
-    // The default (or starting) height for the camera above the player
-    [SerializeField] public float defaultHeightAbovePlayer;
-    // The default (or starting) distance for the camera behind the player
-    [SerializeField] public float defaultDistanceBehindPlayer;
-    // [SerializeField] private float radius;
+    // Target position RELATIVE TO THE PLAYER
+    [SerializeField] private Vector3 targetPos;
 
-    // A zero to one value of where the camera is along its track
-    [SerializeField] private float normalizedCameraPosition;
+    [SerializeField] private float positionUpdateRatio;
 
-    [SerializeField] public float vertMouseSensitivity;
-    [SerializeField] public float maxVerticalRotation;  // Angle on top of player
-    [SerializeField] public float minVerticalRotation;  // Angle underneath player
+    [SerializeField] private float vertMouseSensitivity;
+    [SerializeField] private float scrollMouseSensitivity;
 
-    
+    [SerializeField] private float maxVerticalRotation;
+    [SerializeField] private float minVerticalRotation;
+
+    [SerializeField] private float maxDistanceToPlayer;
+    [SerializeField] private float minDistanceToPlayer;
 
     void Start() {
         // lock cursor
         Cursor.lockState = CursorLockMode.Locked;
+        transform.position = transform.position + targetPos;
+    }
+
+    // Every frame as the last thing to do update the camera's position
+    private void LateUpdate() {
+        // Respond to mouse scrolling
+        Vector3 nextTargetPos = targetPos;
+        nextTargetPos.y += -scrollMouseSensitivity * Input.mouseScrollDelta.y;
+        nextTargetPos.z += scrollMouseSensitivity * Input.mouseScrollDelta.y;
+        if (Input.mouseScrollDelta.y != 0) {
+        }
+        targetPos = nextTargetPos;
+
+        // // Update position by moving towards targetPos
+        // Vector3 hereToTarget = transform.position - targetPos;
+        // Debug.Log(targetPos);
+        // // Debug.Log(hereToTarget);
+        // hereToTarget.Normalize();
+        // Vector3 newPosition = transform.position + positionUpdateRatio * hereToTarget;
+        // Debug.Log(transform.position);
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition,
+        targetPos, (transform.localPosition - targetPos).magnitude * positionUpdateRatio * Time.deltaTime);
+
+        // Rotate the camera based on mouse movement up or down
+        // Reset rotation if it's gone too far
+        Quaternion oldRot = transform.rotation;
+        transform.Rotate(-Input.GetAxis("Mouse Y") * vertMouseSensitivity, 0, 0);
+        float newX = transform.rotation.eulerAngles.x;
+        if (newX > 180 && newX - 360 < minVerticalRotation || newX < 180 && newX > maxVerticalRotation) {
+            transform.rotation = oldRot;
+        }
     }
     
-    void LateUpdate()
-    {
-        Transform updatedTransform = transform;
+    // void LateUpdate()
+    // {
+    //     Transform updatedTransform = transform;
 
-        // updatedTransform.position = player.transform.position - (defaultDistanceBehindPlayer * player.transform.forward) + (defaultHeightAbovePlayer * Vector3.up);
-        // updatedTransform.RotateAround(player.transform.position, player.transform.right, Input.GetAxis("Mouse Y") * vertMouseSensitivity);
-        updatedTransform.position = player.transform.position - (defaultDistanceBehindPlayer * player.transform.forward) + (defaultHeightAbovePlayer * Vector3.up);
-        // updatedTransform.rotation = player.transform.rotation;
+    //     // updatedTransform.position = player.transform.position - (defaultDistanceBehindPlayer * player.transform.forward) + (defaultHeightAbovePlayer * Vector3.up);
+    //     // updatedTransform.RotateAround(player.transform.position, player.transform.right, Input.GetAxis("Mouse Y") * vertMouseSensitivity);
+    //     updatedTransform.position = player.transform.position - (defaultDistanceBehindPlayer * player.transform.forward) + (defaultHeightAbovePlayer * Vector3.up);
+    //     // updatedTransform.rotation = player.transform.rotation;
 
-        // verticalRotationAmount = Mathf.Clamp(verticalRotationAmount - Input.GetAxis("Mouse Y") * verticalRotationSpeed * Time.deltaTime, -maxVerticalRotation, maxVerticalRotation);
-        // updatedTransform.Rotate(0f, verticalRotationAmount, 0f);
+    //     // verticalRotationAmount = Mathf.Clamp(verticalRotationAmount - Input.GetAxis("Mouse Y") * verticalRotationSpeed * Time.deltaTime, -maxVerticalRotation, maxVerticalRotation);
+    //     // updatedTransform.Rotate(0f, verticalRotationAmount, 0f);
 
-        transform.position = updatedTransform.position;
-        // transform.rotation = updatedTransform.rotation;
-    }
+    //     transform.position = updatedTransform.position;
+    //     // transform.rotation = updatedTransform.rotation;
+    // }
 }
