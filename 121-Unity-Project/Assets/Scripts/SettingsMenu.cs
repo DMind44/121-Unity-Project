48a1b71fa0051c16aa3c43f3d285d1372e300c50
serenity-wade <swade@hmc.edu>
2020-04-07 09:56:18 -0700

    feat (UI): Configure settings menu volume slider

--- 121-Unity-Project/Assets/Scripts/SettingsMenu.cs
+++ 121-Unity-Project/Assets/Scripts/SettingsMenu.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetVolume(float volume) {
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
     }
 }
