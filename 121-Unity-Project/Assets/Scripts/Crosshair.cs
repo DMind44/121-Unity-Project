25c27c7859c0b79338d072d258c9f40b5244ceaf
serenity-wade <swade@hmc.edu>
2020-04-07 11:09:24 -0700

    feat (UI): Change crosshair sprite and animation

---
+++ 121-Unity-Project/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public GameObject crosshair;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            crosshair.GetComponent< RectTransform >( ).SetSizeWithCurrentAnchors( RectTransform.Axis.Horizontal, 40);
+            crosshair.GetComponent< RectTransform >( ).SetSizeWithCurrentAnchors( RectTransform.Axis.Vertical, 40);
+        } else {
+            crosshair.GetComponent< RectTransform >( ).SetSizeWithCurrentAnchors( RectTransform.Axis.Horizontal, 50);
+            crosshair.GetComponent< RectTransform >( ).SetSizeWithCurrentAnchors( RectTransform.Axis.Vertical, 50);
+        }
+    }
+}
---
+++ 121-Unity-Project/Assets/Unity UI Pack/Scripts/MainMenuUIHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+using Mirror.Discovery;
+
+public class MainMenuUIHandler : MonoBehaviour {
+
+    public NetworkManager networkManager;  // object that we manipulate to join/leave rooms
+    public NewNetworkDiscovery networkDiscovery;  // object that we manipulate to find/advertise servers
+
+    readonly Dictionary<long, ServerResponse> discoveredServers = new Dictionary<long, ServerResponse>();
+
+    // create a room (as server)
+    public void ServeRoom() {
+        networkManager.StartServer();
+        networkDiscovery.AdvertiseServer();
+    }
+
+    // host a game (serve and play)
+    public void HostRoom() {
+        networkManager.StartHost();
+        networkDiscovery.AdvertiseServer();
+    }
+
+    // join an existing room (as client)
+    public void JoinRoom() {
+        networkDiscovery.StartDiscovery();
+    }
+
+    // This function is referenced in the NetworkDiscovery component and is
+    // called when a server is discovered.
+    // If not connected to a server already, connect to the one we just
+    // discovered.
+    // TODO make this more robust? Stopping discovery altogether with
+    // StopDiscovery() crashes the program. Is this because it shuts down the
+    // NetworkManager, since NetworkManager and NetworkDiscovery are done by
+    // the same object?
+    public void OnDiscoveredServer(DiscoveryResponse info) {
+        // print(networkDiscovery);
+        networkDiscovery.enabled = false;
+        if (!networkManager.isNetworkActive) {
+            networkManager.StartClient(info.uri);
+        }
+    }
+}
